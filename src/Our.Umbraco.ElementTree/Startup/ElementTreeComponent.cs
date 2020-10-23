94758df1427c3250a3581f1b4f12de223f2c375c
Callum Whyte <hey@callumwhyte.com>
2020-10-23 23:11:11 +0100

    Hiding content tree nodes being rendered in element tree

---
+++ src/Our.Umbraco.ElementTree/Startup/ElementTreeComponent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Our.Umbraco.ElementTree.Controllers;
+using Umbraco.Core.Composing;
+using Umbraco.Web.Trees;
+
+namespace Our.Umbraco.ElementTree.Startup
+{
+    public class ElementTreeComponent : IComponent
+    {
+        public void Initialize()
+        {
+            TreeControllerBase.TreeNodesRendering += TreeController_TreeNodesRendering;
+        }
+
+        public void Terminate()
+        {
+            TreeControllerBase.TreeNodesRendering -= TreeController_TreeNodesRendering;
+        }
+
+        private void TreeController_TreeNodesRendering(TreeControllerBase sender, TreeNodesRenderingEventArgs e)
+        {
+            var elementTrees = GetTrees();
+
+            var rootIds = elementTrees.Select(x => x.StartNodeId);
+
+            e.Nodes.RemoveAll(x => rootIds.Contains(x.Id) || rootIds.Contains(x.ParentId));
+        }
+
+        private IEnumerable<IElementTree> GetTrees()
+        {
+            return Current.Factory.GetAllInstances<ElementTreeController>();
+        }
+    }
+}
---
+++ src/Our.Umbraco.ElementTree/Startup/ElementTreeComposer.cs
@@ -0,0 +1,13 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace Our.Umbraco.ElementTree.Startup
+{
+    public class ElementTreeComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.Components().Append<ElementTreeComponent>();
+        }
+    }
+}
