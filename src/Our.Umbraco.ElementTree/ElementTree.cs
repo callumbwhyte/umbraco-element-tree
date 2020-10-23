bffdd12110f81b54db2899352599a9d2ec5706fa
Callum Whyte <hey@callumwhyte.com>
2020-10-23 20:18:17 +0100

    Defining element tree

---
+++ src/Our.Umbraco.ElementTree/ElementTree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Umbraco.Web.Trees;
+
+namespace Our.Umbraco.ElementTree
+{
+    public class ElementTree : Tree, IElementTree
+    {
+        public ElementTree(string sectionAlias, string treeAlias, Type treeControllerType)
+            : this(sectionAlias, treeAlias, treeControllerType, null, null, 0)
+        {
+
+        }
+
+        public ElementTree(IElementTree elementTree)
+            : this(elementTree.SectionAlias, elementTree.TreeAlias, elementTree.TreeControllerType)
+        {
+            StartNodeId = elementTree.StartNodeId;
+        }
+
+        public ElementTree(string sectionAlias, string treeAlias, Type treeControllerType, string treeTitle = null, string treeGroup = null, int sortOrder = 0)
+            : base(sortOrder, sectionAlias, treeGroup, treeAlias, treeTitle, TreeUse.Main | TreeUse.Dialog, treeControllerType, false)
+        {
+            TreeTitle = treeTitle;
+            TreeGroup = treeGroup;
+            SortOrder = sortOrder;
+        }
+
+        public string StartNodeId { get; }
+
+        public new string TreeGroup { get; }
+    }
+}
---
+++ src/Our.Umbraco.ElementTree/IElementTree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Our.Umbraco.ElementTree
+{
+    public interface IElementTree
+    {
+        string SectionAlias { get; }
+
+        string TreeAlias { get; }
+
+        Type TreeControllerType { get; }
+
+        string StartNodeId { get; }
+    }
+}
