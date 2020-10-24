9a265588c4a4012833c90d2f287140252eca36ef
Callum Whyte <hey@callumwhyte.com>
2020-10-24 15:06:16 +0100

    Generic element tree classes

---
+++ src/Our.Umbraco.ElementTree/Controllers/ElementTreeControllerOfT.cs
@@ -0,0 +1,14 @@
+﻿using Umbraco.Web.Trees;
+
+namespace Our.Umbraco.ElementTree.Controllers
+{
+    public abstract class ElementTreeController<T> : ElementTreeController
+        where T : TreeControllerBase
+    {
+        public ElementTreeController(T treeController)
+            : base(treeController)
+        {
+
+        }
+    }
+}
---
+++ src/Our.Umbraco.ElementTree/ElementTreeOfT.cs
@@ -0,0 +1,15 @@
+﻿using Our.Umbraco.ElementTree.Controllers;
+using Umbraco.Web.Trees;
+
+namespace Our.Umbraco.ElementTree
+{
+    public class ElementTree<T> : ElementTree
+        where T : TreeControllerBase
+    {
+        public ElementTree(string sectionAlias, string treeAlias, string startNodeId, string treeTitle = null, string treeGroup = null, int sortOrder = 0)
+            : base(sectionAlias, treeAlias, typeof(ElementTreeController<T>), treeTitle, treeGroup, sortOrder)
+        {
+            StartNodeId = startNodeId;
+        }
+    }
+}
